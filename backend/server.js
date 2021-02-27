6e8947f10e9f04a22488111a64cb6b9ee029ba06
= <2041778+vinicius191@users.noreply.github.com>
2021-02-27 23:17:33 +1100

    ES Modules in Node.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
+const PORT = process.env.PORT || 5000
+const MODE = process.env.NODE_ENV
 
 const app = express()
 
@@ -16,4 +21,4 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`))
