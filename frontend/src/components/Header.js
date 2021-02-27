6774122e7954fbd510f7c2db7475ded70db83286
= <2041778+vinicius191@users.noreply.github.com>
2021-02-27 22:59:27 +1100

    Added backend and moved products json to the backend. Also create 'npm run dev' script to run front and backend at the same time

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+const Header = () => {
+	return (
+		<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+			<Container>
+				<LinkContainer to='/'>
+					<Navbar.Brand>ProShop</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='ml-auto'>
+						<LinkContainer to='/cart'>
+							<Nav.Link>
+								<i className='fas fa-shopping-cart px-2'></i>
+								Cart
+							</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to='/login'>
+							<Nav.Link>
+								<i className='fas fa-user px-2'></i>Sign In
+							</Nav.Link>
+						</LinkContainer>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	)
+}
+
+export default Header
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import axios from 'axios'
+
+const ProductScreen = ({ match }) => {
+	const [product, setProduct] = useState({})
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+			setProduct(data)
+		}
+
+		fetchProduct()
+	}, [match])
+
+	return (
+		<div className='product-screen'>
+			<Link className='btn btn-light my-3' to='/'>
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image
+						src={product.image}
+						alt={product.name}
+						fluid
+						className='padding-bottom-3'
+					/>
+				</Col>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item className='no-padding-top'>
+							<h3 className='no-padding-top'>{product.name}</h3>
+						</ListGroup.Item>
+					</ListGroup>
+					<ListGroup.Item>
+						<Rating
+							value={product.rating}
+							text={`${product.numReviews} reviews`}
+						/>
+					</ListGroup.Item>
+					<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+					<ListGroup.Item>
+						Description: ${product.description}
+					</ListGroup.Item>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>Price:</Col>
+									<Col>
+										<strong>{product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										{product.countInStock > 0
+											? 'In stock'
+											: 'Out of stock'}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									className='btn-block'
+									type='button'
+									disabled={product.countInStock === 0}
+								>
+									Add to cart
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</div>
+	)
+}
+
+export default ProductScreen
